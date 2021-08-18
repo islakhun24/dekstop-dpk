9885944cf06e5549b69d30b79c0200795d79394d
islakhunnurdzaki22 <islakhunnurdzaki22@gmail.com>
2021-08-18 08:44:23 +0700

    make it betters

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'http://localhost:8080/api/',
-  // base_url: 'http://regulatedagen.fiprodigital.com/api/'
+  // base_url: 'http://localhost:8080/api/',
+  // base_url: 'http://regulatedagen.fiprodigital.com/api/',
+  base_url: 'https://regulated-agen-backend.herokuapp.com/api/',
 };
